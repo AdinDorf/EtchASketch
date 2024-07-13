014034a3dc70206f77cfb01683555591df2345f0
AdinDorf <adin@dorffamily.com>
2024-07-13 14:38:29 -0500

    Hover grid works!

--- script.js
+++ script.js
@@ -4,5 +4,20 @@ const grid = document.querySelector("#container");
 for(let i = 0; i < 16; i++) {
 const gridSquare = document.createElement("div");
 gridSquare.classList.add("square");
+gridSquare.addEventListener("mouseover", () =>
+    {
+        gridSquare.style.backgroundColor = 'green';
+    }
+);
+gridSquare.addEventListener("mouseout", () =>
+    {
+        gridSquare.style.backgroundColor = 'red';
+    }
+);
 grid.appendChild(gridSquare);
+}
+
+function onHover()
+{
+    
 }
