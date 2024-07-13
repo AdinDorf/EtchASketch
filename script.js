163ce6b1a2c3f01eb69b4d7e4be55f62d5be26f8
AdinDorf <adin@dorffamily.com>
2024-07-13 14:20:12 -0500

    Made Grid

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const grid = document.querySelector("#container");
+
+
+for(let i = 0; i < 16; i++) {
 const gridSquare = document.createElement("div");
 gridSquare.classList.add("square");
 grid.appendChild(gridSquare);
+}
