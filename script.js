2ed8c444c51f1b9170bd6880c3722ea972c849fc
AdinDorf <adin@dorffamily.com>
2024-07-13 17:53:40 -0500

    Finally got this stupid grid working

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 const grid = document.querySelector("#container");
+const gridSize = 10000;
 
+createGrid();
 
-for(let i = 0; i < 16; i++) {
-const gridSquare = document.createElement("div");
-gridSquare.classList.add("square");
-gridSquare.addEventListener("mouseover", () =>
-    {
-        gridSquare.style.backgroundColor = 'green';
-    }
-);
-gridSquare.addEventListener("mouseout", () =>
-    {
-        gridSquare.style.backgroundColor = 'red';
-    }
-);
-grid.appendChild(gridSquare);
-}
-
-function onHover()
+function createGrid()
 {
-    
+    grid.innerHTML='';
+    const squareSize = Math.floor(Math.sqrt(gridSize));
+    console.log(squareSize);
+    for(let i = 0; i < gridSize; i++) {
+        
+        const gridSquare = document.createElement("div");
+        
+        gridSquare.classList.add("square");
+        gridSquare.style.width = `calc(100% / ${squareSize} )`;
+        gridSquare.style.height = `calc(100% / ${squareSize} )`;
+
+
+        gridSquare.addEventListener("mouseover", () =>
+            {
+                gridSquare.style.backgroundColor = 'green';
+            }
+        );
+        gridSquare.addEventListener("mouseout", () =>
+            {
+                gridSquare.style.backgroundColor = 'red';
+            }
+        );
+        grid.appendChild(gridSquare);
+        }
 }
